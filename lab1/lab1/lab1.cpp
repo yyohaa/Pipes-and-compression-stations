f642d414657638eabe6ce20e5181398854000da7
yoha <Windows@YOHA>
2026-09-07 14:44:19 +0300

    структура и харак данных

---
+++ lab1/lab1/lab1.cpp
@@ -0,0 +1,29 @@
+﻿// lab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+struct Pipe {
+    string name;
+    double len;
+    int d;
+    bool state;
+};
+
+struct CS {
+    string name;
+    int squant;
+    int wsquant;
+    string type;
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
